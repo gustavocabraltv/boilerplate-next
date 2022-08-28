c0f4de1e24fef00e0288a5c4ea128e550d0d2a14
Gustavo Cabral <studiogustavocabral@gmail.com>
2022-08-28 18:07:01 -0300

    Heading added

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,8 +1,32 @@
 import * as S from './styles'
 
-const Heading = () => (
-  <S.Wrapper>
-    <h1>Heading</h1>
+export type LineColors = 'primary' | 'secondary'
+
+export type HeadingProps = {
+  children: React.ReactNode
+  color?: 'white' | 'black'
+  lineLeft?: boolean
+  lineBottom?: boolean
+  lineColor?: LineColors
+  size?: 'small' | 'medium' | 'huge'
+}
+
+const Heading = ({
+  children,
+  color = 'white',
+  lineLeft = false,
+  lineBottom = false,
+  lineColor = 'primary',
+  size = 'medium'
+}: HeadingProps) => (
+  <S.Wrapper
+    color={color}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    lineColor={lineColor}
+    size={size}
+  >
+    {children}
   </S.Wrapper>
 )
 
