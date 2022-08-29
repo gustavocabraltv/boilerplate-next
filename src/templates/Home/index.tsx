3444be89a024e31de81c995175a519e8364dc419
Usuario <usuario@MacBook-Pro-de-Heitor.local>
2022-08-29 02:25:47 -0300

    Banner Slider

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,12 +1,17 @@
 import Button from 'components/Button'
 import { Container } from 'components/Container'
 import Menu from 'components/Menu'
+import Heading from 'components/Heading'
 
 const Home = () => (
   <section>
     <Container>
       <Menu />
       <Button>sadasd</Button>
+    </Container>
+
+    <Container>
+      <Heading lineColor="secondary">Most Populars</Heading>
     </Container>
   </section>
 )
